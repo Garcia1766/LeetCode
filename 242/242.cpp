401e21bd73bfbd23d08aea93df09f16776cef565
Garcia1766 <liyangkun17@gmail.com>
2020-08-02 11:02:43 +0800

    242

---
+++ 242/242.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<ctime>
+#include<cassert>
+#include<cmath>
+#include<string>
+#include<map>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length())
+            return false;
+        
+        map<char, int> map1;
+        for (int i = 0; i < s.length(); i++) {
+            map1[s[i]]++;
+        }
+        int unbalance = s.length();
+
+        for (int i = 0; i < t.length(); i++) {
+            if (map1[t[i]] > 0) {
+                map1[t[i]]--;
+                unbalance--;
+            }
+        }
+        return !unbalance;
+    }
+};
+
+int main() {
+    string s = "rat", t = "car";
+    cout << Solution().isAnagram(s, t) << endl;
+
+    return 0;
+}
