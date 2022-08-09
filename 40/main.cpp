a2f6d0d6a26a37418b5b6c934bcbcb76d793b7e2
Garcia1766 <liyangkun17@gmail.com>
2022-08-10 00:26:19 +0800

    8-5 回溯法解决组合问题的优化 39, 40

--- 40/main.cpp
+++ 40/main.cpp
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-void print_vector(vector<int> vec, bool newline = false){
+void print_vector(vector<int> vec, bool newline = true){
     printf("[");
     for (int i = 0; i < vec.size(); i++)
         printf("%d,", vec[i]);
@@ -24,16 +24,22 @@ private:
             res.push_back(stored);
             return;
         }
-        for (int i = start; i < candidates.size(); i++) {
+        // for循环每次更新i不是加1，而是跳至下一个值的第一个位置
+        for (int i = start; i < candidates.size();) {
             if (candidates[i] > target) break;
+
             stored.push_back(candidates[i]);
-            generateCombination(candidates, target - candidates[i], i, stored);  // 第3个参数传入i而非i+1，意为允许重复使用元素
+            generateCombination(candidates, target - candidates[i], i + 1, stored);
             stored.pop_back();
+
+            while (i < candidates.size() - 1 && candidates[i + 1] == candidates[i])
+                i++;
+            i++;
         }
         return;
     }
 public:
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         sort(candidates.begin(), candidates.end());  // 默认从小到大
         vector<int> stored;
         generateCombination(candidates, target, 0, stored);
@@ -42,9 +48,9 @@ public:
 };
 
 int main() {
-    vector<int> candidates = {2,3,6,7};
-    int target = 7;
-    vector<vector<int>> res = Solution().combinationSum(candidates, target);
+    vector<int> candidates = {10,1,2,7,6,1,5};
+    int target = 8;
+    vector<vector<int>> res = Solution().combinationSum2(candidates, target);
     for (int i = 0; i < res.size(); i++) {
         print_vector(res[i], false);
         printf(", ");
