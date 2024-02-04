8738322c4746315a4e88e398b733560fb8b450f8
Garcia1766 <liyangkun17@gmail.com>
2024-02-03 21:31:15 -0500

    187 not completed

---
+++ 187/Main.java
@@ -0,0 +1,82 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+class Solution {
+    Set<Integer> posSet = new HashSet<>();
+    Set<String> strSet = new HashSet<>();
+
+    boolean KMpSearch(String pat, String txt, int start_pos) {
+        int M = pat.length(), N = txt.length();
+        int[] lps = computeLPSArray(pat);
+        int i = 0, j = 0;
+        int foundTimes = 0;
+        while ((N - i) >= (M - j)) {
+            if (pat.charAt(j) == txt.charAt(i)) {
+                j++;
+                i++;
+            }
+            if (j == M) {
+                if (posSet.contains(i - j)) {
+                    return false;
+                } else {
+                    posSet.add(i - j);
+                    foundTimes++;
+                    j = lps[j - 1];
+                }
+            } else if (i < N && pat.charAt(j) != txt.charAt(i)) {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
+            }
+        }
+        return foundTimes > 1;
+    }
+
+    int[] computeLPSArray(String pat) {
+        int M = pat.length();
+        int len = 0;
+        int i = 1;
+        int[] lps = new int[M];
+
+        while (i < M) {
+            if (pat.charAt(i) == pat.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = len;
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
+
+    public List<String> findRepeatedDnaSequences(String s) {
+        List<String> res = new ArrayList<>();
+        for (int i = 0; i <= s.length() - 10; i++) {
+            String pat = s.substring(i, i + 10);
+            if (strSet.contains(pat)) continue;
+            strSet.add(pat);
+            if (KMpSearch(pat, s, i)) {
+                res.add(pat);
+            }
+        }
+        return res;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        String s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
+        List<String> res = new Solution().findRepeatedDnaSequences(s);
+        System.out.println(res);
+    }
+}
---
+++ 187/Main2.java
@@ -0,0 +1,76 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+class Solution {
+    Map<Character, Integer> c2iMap;
+    Map<Integer, Character> i2cMap;
+    Set<Integer> patSet;
+
+    Solution() {
+        c2iMap = new HashMap<>();
+        c2iMap.put('A', 0);
+        c2iMap.put('C', 1);
+        c2iMap.put('G', 2);
+        c2iMap.put('T', 3);
+
+        i2cMap = new HashMap<>();
+        i2cMap.put(0, 'A');
+        i2cMap.put(1, 'C');
+        i2cMap.put(2, 'G');
+        i2cMap.put(3, 'T');
+
+        patSet = new HashSet<>();
+    }
+
+    int string2Int(String dna) { // dna.length()==10
+        int res = 0;
+        for (int i = 0; i < dna.length(); ++i) {
+            res = (res << 2) + c2iMap.get(dna.charAt(i));
+        }
+        return res;
+    }
+
+    String int2String(int val) {
+        StringBuilder sbd = new StringBuilder();
+        for (int i = 0; i < 10; ++i) {
+            sbd.append(i2cMap.get(val % 4));
+            val >>= 2;
+        }
+        return sbd.reverse().toString();
+    }
+
+    boolean searchPattern(int pat, String s, int start_pos) {
+        for (int i = 0; i <= s.length() - 10; ++i) {
+            if (i == start_pos) continue;
+            if (string2Int(s.substring(i, i + 10)) == pat) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public List<String> findRepeatedDnaSequences(String s) {
+        List<String> res = new ArrayList<>();
+        for (int i = 0; i <= s.length() - 10; i++) {
+            int pat = string2Int(s.substring(i, i + 10));
+            if (patSet.contains(pat)) continue;
+            patSet.add(pat);
+            if (searchPattern(pat, s, i)) {
+                res.add(s.substring(i, i + 10));
+            }
+        }
+        return res;
+    }
+}
+
+public class Main2 {
+    public static void main(String[] args) {
+        String s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
+        List<String> res = new Solution().findRepeatedDnaSequences(s);
+        System.out.println(res);
+    }
+}
